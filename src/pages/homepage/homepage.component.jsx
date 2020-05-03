c2d04507766fc53ba076530e07eb2e0808990b1f
Rohit Tiwari <rohittiwarirvt@gmail.com>
2020-05-03 11:25:37 +0530

    conversted simple homepage, menu direcoty and direcotry to componenets

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import  "./homepage.style.scss";
+import MenuDirectory from "../../components/menu-directory/menu-directory.component";
+
+const HomePage = () => (
+    <div className="homepage">
+        <MenuDirectory></MenuDirectory>
+    </div>
+)
+
+export default HomePage;
