9b952ccdf59aa4878d9e396dd7f03fa63d2ed319
Rohit Tiwari <rohittiwarirvt@gmail.com>
2020-05-08 12:47:50 +0530

    Added signin page redirect with clean up for actions types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from  'react-redux';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -45,7 +45,12 @@ class  App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
           <Route  path="/shop" component={ShopPage}></Route>
-          <Route path="/signin" component={SigninSignupPage}></Route>
+          <Route exact path="/signin" render={() =>
+            this.props.currentUser ?
+             (<Redirect to="/"></Redirect>):
+             (<SigninSignupPage/>)
+          }
+          ></Route>
         </Switch>
       </div>
     );
@@ -53,8 +58,12 @@ class  App extends React.Component {
 
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser:user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
